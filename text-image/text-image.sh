9294dd5ac694317c10f3bf23386a6375f7305bc7
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2024-08-22 11:14:16 +0200

    text-image.sh Add delay and out filename parameters

--- text-image/text-image.sh
+++ text-image/text-image.sh
@@ -8,8 +8,10 @@ color2="$3"
 text1="$4"
 text2="$5"
 font="$6"
+delay="$7"
+out_filename="$8"
 
-shift 6
+shift 8
 
 magick -size "$size" -background "$color1" -fill "$color2" -font "$font" -gravity center label:"$text1" "$tmp_dir/text1.gif"
 magick -size "$size" -background "$color2" -fill "$color1" -font "$font" -gravity center label:"$text2" "$tmp_dir/text2.gif"
@@ -24,6 +26,6 @@ for img in "$@"; do
   magick "$img" -resize "$size^" -gravity center -crop "$size+0+0" +repage "$tmp_dir/$(printf 'frame%03d.gif' $frame)"
 done
 
-magick -size "$size" -delay 100 -loop 0 "$tmp_dir/frame"* out.gif
+magick -size "$size" -delay "$delay" -loop 0 "$tmp_dir/frame"* "$out_filename"
 
 rm -rf "$tmp_dir"
