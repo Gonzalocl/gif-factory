9e042f84a42e641cc271c63945777680ba4dd8e7
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2025-11-30 17:05:13 +0100

    gif-collage.sh

--- gif-collage/gif-collage.sh
+++ gif-collage/gif-collage.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 input_file="$1"
-width_tiles=4
-height_tiles=4
-width_pixels=468
+width_tiles="${2:-5}"
+height_tiles="${3:-5}"
+width_pixels="${4:-500}"
 
 total_tiles=$((width_tiles*height_tiles))
 filter_inputs="$(python -c "print('[0]' * ${total_tiles})")"
+output_filename="$(basename "${input_file}")__${width_tiles}x${height_tiles}-${width_pixels}.mp4"
 
 ffmpeg -i "${input_file}" \
     -filter_complex "${filter_inputs}xstack=grid=${width_tiles}x${height_tiles}[v];[v]scale=width=${width_pixels}:height=-1" \
-    out.mp4
+    "${output_filename}"
